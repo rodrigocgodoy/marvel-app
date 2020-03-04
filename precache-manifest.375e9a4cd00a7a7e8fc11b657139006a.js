621eb7754369b65e6a8ba2a1097fae6a4a93ff3d
Rodrigo Godoy MacBook <rodrigocgodoy@hotmail.com>
2020-03-04 00:34:37 -0300

    Updates

---
+++ precache-manifest.375e9a4cd00a7a7e8fc11b657139006a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e42daecdc96e4c4bcbdcd3e27859c0",
+    "url": "/softplan-desafio/index.html"
+  },
+  {
+    "revision": "3195ab24352943017940",
+    "url": "/softplan-desafio/static/js/2.15c07c9d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/softplan-desafio/static/js/2.15c07c9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f968ba46e5d32b447fe8",
+    "url": "/softplan-desafio/static/js/main.6081c646.chunk.js"
+  },
+  {
+    "revision": "23b0fb9c1fc3d1fe9574",
+    "url": "/softplan-desafio/static/js/runtime-main.6d6f5853.js"
+  },
+  {
+    "revision": "2f599a7d406202a8aa25ebf9c8bcbcb3",
+    "url": "/softplan-desafio/static/media/logo.2f599a7d.png"
+  }
+]);
